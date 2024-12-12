ef1a8641f4151e2110593291df02169bae2ac4e2
fluxdiv <156196590+fluxdiv@users.noreply.github.com>
2024-12-12 18:28:49 -0500

    update templates and readme to new schema

--- lua/relative-rainbow/config/templates/rainbow.lua
+++ lua/relative-rainbow/config/templates/rainbow.lua
@@ -2,10 +2,18 @@ local M = {}
 
 -- Rainbow line dark
 M.rainbow_line_dark = {
-  { distance_from_cursor = 5, bg_tint = "#3D0300", tint_multiplier = 0.1, fill = false }, -- Dark red
-  { distance_from_cursor = 11, bg_tint = "#3a3d00", tint_multiplier = 0.1, fill = false }, -- Dark yellow
-  { distance_from_cursor = 17, bg_tint = "#003d0b", tint_multiplier = 0.1, fill = false }, -- Dark green
-  { distance_from_cursor = 22, bg_tint = "#00313d", tint_multiplier = 0.1, fill = false }, -- Dark blue
+  { distance_from_cursor = 5, bg_tint = "#3D0300", bg_tint_multiplier = 0.1, fill = false }, -- Dark red
+  { distance_from_cursor = 11, bg_tint = "#3a3d00", bg_tint_multiplier = 0.1, fill = false }, -- Dark yellow
+  { distance_from_cursor = 17, bg_tint = "#003d0b", bg_tint_multiplier = 0.1, fill = false }, -- Dark green
+  { distance_from_cursor = 22, bg_tint = "#00313d", bg_tint_multiplier = 0.1, fill = false }, -- Dark blue
+}
+
+-- Rainbow fill dark
+M.rainbow_fill_dark = {
+  { distance_from_cursor = 5, bg_tint = "#3D0300", bg_tint_multiplier = 0.1 }, -- Dark red
+  { distance_from_cursor = 11, bg_tint = "#3a3d00", bg_tint_multiplier = 0.1 }, -- Dark yellow
+  { distance_from_cursor = 17, bg_tint = "#003d0b", bg_tint_multiplier = 0.1 }, -- Dark green
+  { distance_from_cursor = 22, bg_tint = "#00313d", bg_tint_multiplier = 0.1 }, -- Dark blue
 }
 
 -- Inner red, outer blue
@@ -22,14 +30,14 @@ M.rainbow_red_solid = {
 
 -- Inner red, outer blue
 M.rainbow_red_tint = {
-  { distance_from_cursor = 3, bg_tint = "#FF0000", tint_multiplier = 0.1, fill = true }, -- Red
-  { distance_from_cursor = 6, bg_tint = "#FF7F00", tint_multiplier = 0.1, fill = true }, -- Orange
-  { distance_from_cursor = 9, bg_tint = "#FFFF00", tint_multiplier = 0.1, fill = true }, -- Yellow
-  { distance_from_cursor = 12, bg_tint = "#00FF00", tint_multiplier = 0.1, fill = true }, -- Green
-  { distance_from_cursor = 15, bg_tint = "#0000FF", tint_multiplier = 0.1, fill = true }, -- Blue
-  { distance_from_cursor = 18, bg_tint = "#4B0082", tint_multiplier = 0.1, fill = true }, -- Indigo
-  { distance_from_cursor = 21, bg_tint = "#8B00FF", tint_multiplier = 0.1, fill = true }, -- Violet
-  { distance_from_cursor = 24, bg_tint = "#8B00FF", tint_multiplier = 0.1, fill = true }, -- Slightly lighter Violet
+  { distance_from_cursor = 3, bg_tint = "#FF0000", bg_tint_multiplier = 0.1, fill = true }, -- Red
+  { distance_from_cursor = 6, bg_tint = "#FF7F00", bg_tint_multiplier = 0.1, fill = true }, -- Orange
+  { distance_from_cursor = 9, bg_tint = "#FFFF00", bg_tint_multiplier = 0.1, fill = true }, -- Yellow
+  { distance_from_cursor = 12, bg_tint = "#00FF00", bg_tint_multiplier = 0.1, fill = true }, -- Green
+  { distance_from_cursor = 15, bg_tint = "#0000FF", bg_tint_multiplier = 0.1, fill = true }, -- Blue
+  { distance_from_cursor = 18, bg_tint = "#4B0082", bg_tint_multiplier = 0.1, fill = true }, -- Indigo
+  { distance_from_cursor = 21, bg_tint = "#8B00FF", bg_tint_multiplier = 0.1, fill = true }, -- Violet
+  { distance_from_cursor = 24, bg_tint = "#8B00FF", bg_tint_multiplier = 0.1, fill = true }, -- Slightly lighter Violet
 }
 
 -- Inner blue, outer red
@@ -46,14 +54,14 @@ M.rainbow_blue_solid = {
 
 -- Inner blue, outer red
 M.rainbow_blue_tint = {
-  { distance_from_cursor = 3, bg_tint = "#0000FF", tint_multiplier = 0.1, fill = true }, -- Blue
-  { distance_from_cursor = 6, bg_tint = "#00FF00", tint_multiplier = 0.1, fill = true }, -- Green
-  { distance_from_cursor = 9, bg_tint = "#FFFF00", tint_multiplier = 0.1, fill = true }, -- Yellow
-  { distance_from_cursor = 12, bg_tint = "#FF7F00", tint_multiplier = 0.1, fill = true }, -- Orange
-  { distance_from_cursor = 15, bg_tint = "#FF0000", tint_multiplier = 0.1, fill = true }, -- Red
-  { distance_from_cursor = 18, bg_tint = "#8B00FF", tint_multiplier = 0.1, fill = true }, -- Violet
-  { distance_from_cursor = 21, bg_tint = "#4B0082", tint_multiplier = 0.1, fill = true }, -- Indigo
-  { distance_from_cursor = 24, bg_tint = "#4B0082", tint_multiplier = 0.1, fill = true }, -- Slightly lighter Indigo
+  { distance_from_cursor = 3, bg_tint = "#0000FF", bg_tint_multiplier = 0.1, fill = true }, -- Blue
+  { distance_from_cursor = 6, bg_tint = "#00FF00", bg_tint_multiplier = 0.1, fill = true }, -- Green
+  { distance_from_cursor = 9, bg_tint = "#FFFF00", bg_tint_multiplier = 0.1, fill = true }, -- Yellow
+  { distance_from_cursor = 12, bg_tint = "#FF7F00", bg_tint_multiplier = 0.1, fill = true }, -- Orange
+  { distance_from_cursor = 15, bg_tint = "#FF0000", bg_tint_multiplier = 0.1, fill = true }, -- Red
+  { distance_from_cursor = 18, bg_tint = "#8B00FF", bg_tint_multiplier = 0.1, fill = true }, -- Violet
+  { distance_from_cursor = 21, bg_tint = "#4B0082", bg_tint_multiplier = 0.1, fill = true }, -- Indigo
+  { distance_from_cursor = 24, bg_tint = "#4B0082", bg_tint_multiplier = 0.1, fill = true }, -- Slightly lighter Indigo
 }
 
 return M
